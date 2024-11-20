e11ae84aaaf999e63f73b5c7fc902e607e39b27e
MD SIFATULLAH SHEIKH <103960510+SifatSwapnil2022@users.noreply.github.com>
2024-11-20 23:15:44 +0600

    Create Chef Plays Ludo.cpp

---
+++ Chef Plays Ludo.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x==6){
+	        std::cout << "yes" << std::endl;
+	    }
+	    else{
+	        cout<<"no"<<endl;
+	    }
+	}
+
+}
