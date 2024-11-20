f1e0e0a7cddaff9cd190043f8355944f4c63f7c7
MD SIFATULLAH SHEIKH <103960510+SifatSwapnil2022@users.noreply.github.com>
2024-11-20 23:14:54 +0600

    Create Masterchef finals.cpp

---
+++ Masterchef finals.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+ int t;
+ cin>>t;
+ while(t--){
+     int c;
+     cin>>c;
+     if(c<11){
+         std::cout << "Yes" << std::endl;
+     }
+     else{
+         std::cout << "No" << std::endl;
+     }
+ }
+}
