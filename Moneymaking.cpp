0d9bff522f4b8de3203ef7578c6ee31466653682
MD SIFATULLAH SHEIKH <103960510+SifatSwapnil2022@users.noreply.github.com>
2024-11-20 23:13:25 +0600

    Create Moneymaking.cpp

---
+++ Moneymaking.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x_nug,y_star;
+	cin>>x_nug>>y_star;
+	int sum = x_nug*5000 + y_star*9800;
+	std::cout << sum << std::endl;
+	return 0;
+	
+
+}
